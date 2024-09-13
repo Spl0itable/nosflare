14c672203593d1cc8a4cb30c8539ace5f023668c
LA <lucas@censorship.rip>
2024-09-13 15:27:40 +0930

    v4.20.23

--- relay-worker.js
+++ relay-worker.js
@@ -10,7 +10,7 @@ const relayInfo = {
     contact: "lux@censorship.rip",
     supported_nips: [1, 2, 4, 5, 9, 11, 12, 15, 16, 17, 20, 22, 33, 40],
     software: "https://github.com/Spl0itable/nosflare",
-    version: "4.20.22",
+    version: "4.20.23",
 };
 
 // Relay favicon
