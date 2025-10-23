2df28bdd5d2cbbca4b159ed10d8fcc9109bc08af
LA <lucas@censorship.rip>
2025-10-23 14:52:39 +1030

    Revert "v7.4.15"
    This reverts commit a35c643236eb818bec6f672df88fc36a07b5c589.

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const relayInfo: RelayInfo = {
   contact: "lux@fed.wtf",
   supported_nips: [1, 2, 4, 5, 9, 11, 12, 15, 16, 17, 20, 22, 33, 40],
   software: "https://github.com/Spl0itable/nosflare",
-  version: "7.4.15",
+  version: "7.4.14",
   icon: "https://raw.githubusercontent.com/Spl0itable/nosflare/main/images/flare.png",
 
   // Optional fields (uncomment as needed):
@@ -149,8 +149,8 @@ export const allowedTags = new Set<string>([
 ]);
 
 // Rate limit thresholds
-export const PUBKEY_RATE_LIMIT = { rate: 10 / 60000, capacity: 10 }; // 10 EVENT messages per min
-export const REQ_RATE_LIMIT = { rate: 50 / 60000, capacity: 50 }; // 50 REQ messages per min
+export const PUBKEY_RATE_LIMIT = { rate: 50 / 60000, capacity: 50 }; // 50 EVENT messages per min
+export const REQ_RATE_LIMIT = { rate: 500 / 60000, capacity: 500 }; // 500 REQ messages per min
 export const excludedRateLimitKinds = new Set<number>([
   1059
   // ... kinds to exclude from EVENT rate limiting Ex: 1, 2, 3
